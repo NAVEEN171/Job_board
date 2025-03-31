e27b9e74e29954ced4f0ce38c08b0c59c1b85f43
NAVEEN171 <naveenkumar171837@gmail.com>
2025-03-31 22:59:55 +0530

    fixed bugs

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -3,11 +3,14 @@
 import { Provider } from "react-redux";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import store from "@/store";
+import { Suspense } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
-      <Provider store={store}>{children}</Provider>
-    </GoogleOAuthProvider>
+    <Suspense>
+      <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+        <Provider store={store}>{children}</Provider>
+      </GoogleOAuthProvider>
+    </Suspense>
   );
 }
