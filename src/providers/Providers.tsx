7330bc183d411ed8df418c819abf408729c3fb5d
NAVEEN171 <naveenkumar171837@gmail.com>
2025-04-01 23:48:40 +0530

    fixed suspense bug

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -5,9 +5,9 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import store from "@/store";
 import { Suspense } from "react";
 
-export default function Providers({ children }: { children: React.ReactNode }) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <Suspense fallback={<div></div>}>
+    <Suspense>
       <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
         <Provider store={store}>{children}</Provider>
       </GoogleOAuthProvider>
