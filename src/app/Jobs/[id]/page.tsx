3fc3a146dca3a10b28d9ab204a644f4bebf60737
NAVEEN171 <naveenkumar171837@gmail.com>
2025-04-02 23:25:14 +0530

    updated footer

--- src/app/Jobs/[id]/page.tsx
+++ src/app/Jobs/[id]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useId, useState } from "react";
+import React, { useEffect, useId, useState, Fragment } from "react";
 import {
   formatDistanceToNow,
   differenceInDays,
@@ -12,6 +12,7 @@ import { Authactions } from "@/store/Substores/Authslice";
 import OvalLoader from "@/components/ovalLoader/ovalLoader";
 import store from "@/store";
 import Navbar from "@/components/Navbar/Navbar";
+import Footer from "../../../components/Footer/Footer";
 
 interface JobPageProps {
   params: Promise<{
@@ -81,113 +82,118 @@ const page = ({ params }: JobPageProps) => {
     <div className={`w-full ${loading ? "h-screen" : ""} mb-5 `}>
       <Navbar />
       {currentJob && !loading && (
-        <div className="w-full flex justify-center">
-          <div className="w-[80%] flex flex-col gap-3">
-            <div className="font-bold text-2xl">{currentJob.job_title}</div>
-            <div className="text-gray-400 font-medium">
-              {formatDate(currentJob.date_posted)}
-            </div>
-            <div className="company-urls flex xs:font-sm  font-medium text-base gap-[10px]">
-              {currentJob?.company_link && (
-                <Link
-                  className="px-[10px] py-[5px] rounded-[5px]  bg-[#EFF8FF] text-[#3A90FF]   border-[2px] border-[#B2DDFF] "
-                  target="_blank"
-                  href={
-                    currentJob.company_link?.startsWith("http")
-                      ? currentJob.company_link
-                      : currentJob.company_link.startsWith("www.")
-                      ? `https://${currentJob.company_link}`
-                      : `https://www.${currentJob.company_link}`
-                  }
-                >
-                  Company
-                </Link>
-              )}
-              {currentJob.company_data?.linkedin_link && (
-                <Link
-                  className="px-[10px] py-[5px] rounded-[5px] xs:font-xs bg-[#EFF8FF] text-[#3A90FF]  border-2 border-[#B2DDFF] "
-                  target="_blank"
-                  href={currentJob.company_data.linkedin_link}
-                >
-                  Linkedin
-                </Link>
-              )}
-            </div>
-            <div className="flex gap-[10px] font-medium xs:font-xs text-base w-full md:max-w-[80%] flex-wrap">
-              {currentJob.job_board && (
-                <div className="px-[10px] py-[5px] cursor-pointer rounded-[5px] border border-1 border-[#C8C8C8]">
-                  {currentJob.job_board}
-                </div>
-              )}
-              {currentJob.job_type && (
-                <div className="px-[10px] py-[5px] cursor-pointer rounded-[5px] border border-1 border-[#C8C8C8]">
-                  {currentJob.job_type}
-                </div>
-              )}
-              {currentJob.location_type && (
-                <div className="px-[10px] py-[5px] cursor-pointer rounded-[5px] border border-1 border-[#C8C8C8]">
-                  {currentJob.location_type}
-                </div>
-              )}
-              <div className="flex gap-[10px] flex-wrap">
-                {currentJob.company_data?.industries &&
-                  currentJob.company_data.industries
-                    .filter((industry: string) => industry.trim() !== "")
-                    .map((industry: string) => (
-                      <div
-                        key={industry}
-                        className="px-[10px] cursor-pointer py-[5px] rounded-[5px] border  border-1 border-[#C8C8C8]"
-                      >
-                        {industry}
-                      </div>
-                    ))}
-                {currentJob.company_data?.subindustries &&
-                  currentJob.company_data.subindustries
-                    .filter((industry: string) => industry.trim() !== "")
-                    .map((industry: string) => (
-                      <div
-                        key={industry}
-                        className="px-[10px] cursor-pointer py-[5px] rounded-[5px] border border-1 border-[#C8C8C8]"
-                      >
-                        {industry}
-                      </div>
-                    ))}
+        <Fragment>
+          <div className="w-full flex justify-center">
+            <div className="w-[80%] flex flex-col gap-3">
+              <div className="font-bold text-2xl">{currentJob.job_title}</div>
+              <div className="text-gray-400 font-medium">
+                {formatDate(currentJob.date_posted)}
               </div>
-            </div>
-            <div className="flex flex-wrap gap-4">
-              <Link
-                href="/"
-                className="px-[10px] cursor-pointer font-medium text-base xs:text-sm py-[6px] rounded-md border  border-1 border-[#C8C8C8]"
-              >
-                🔍 Search all jobs
-              </Link>
+              <div className="company-urls flex xs:font-sm  font-medium text-base gap-[10px]">
+                {currentJob?.company_link && (
+                  <Link
+                    className="px-[10px] py-[5px] rounded-[5px]  bg-[#EFF8FF] text-[#3A90FF]   border-[2px] border-[#B2DDFF] "
+                    target="_blank"
+                    href={
+                      currentJob.company_link?.startsWith("http")
+                        ? currentJob.company_link
+                        : currentJob.company_link.startsWith("www.")
+                        ? `https://${currentJob.company_link}`
+                        : `https://www.${currentJob.company_link}`
+                    }
+                  >
+                    Company
+                  </Link>
+                )}
+                {currentJob.company_data?.linkedin_link && (
+                  <Link
+                    className="px-[10px] py-[5px] rounded-[5px] xs:font-xs bg-[#EFF8FF] text-[#3A90FF]  border-2 border-[#B2DDFF] "
+                    target="_blank"
+                    href={currentJob.company_data.linkedin_link}
+                  >
+                    Linkedin
+                  </Link>
+                )}
+              </div>
+              <div className="flex gap-[10px] font-medium xs:font-xs text-base w-full md:max-w-[80%] flex-wrap">
+                {currentJob.job_board && (
+                  <div className="px-[10px] py-[5px] cursor-pointer rounded-[5px] border border-1 border-[#C8C8C8]">
+                    {currentJob.job_board}
+                  </div>
+                )}
+                {currentJob.job_type && (
+                  <div className="px-[10px] py-[5px] cursor-pointer rounded-[5px] border border-1 border-[#C8C8C8]">
+                    {currentJob.job_type}
+                  </div>
+                )}
+                {currentJob.location_type && (
+                  <div className="px-[10px] py-[5px] cursor-pointer rounded-[5px] border border-1 border-[#C8C8C8]">
+                    {currentJob.location_type}
+                  </div>
+                )}
+                <div className="flex gap-[10px] flex-wrap">
+                  {currentJob.company_data?.industries &&
+                    currentJob.company_data.industries
+                      .filter((industry: string) => industry.trim() !== "")
+                      .map((industry: string) => (
+                        <div
+                          key={industry}
+                          className="px-[10px] cursor-pointer py-[5px] rounded-[5px] border  border-1 border-[#C8C8C8]"
+                        >
+                          {industry}
+                        </div>
+                      ))}
+                  {currentJob.company_data?.subindustries &&
+                    currentJob.company_data.subindustries
+                      .filter((industry: string) => industry.trim() !== "")
+                      .map((industry: string) => (
+                        <div
+                          key={industry}
+                          className="px-[10px] cursor-pointer py-[5px] rounded-[5px] border border-1 border-[#C8C8C8]"
+                        >
+                          {industry}
+                        </div>
+                      ))}
+                </div>
+              </div>
+              <div className="flex flex-wrap gap-4">
+                <Link
+                  href="/"
+                  className="px-[10px] cursor-pointer font-medium text-base xs:text-sm py-[6px] rounded-md border  border-1 border-[#C8C8C8]"
+                >
+                  🔍 Search all jobs
+                </Link>
+                <Link
+                  href={`${currentJob.application_link}`}
+                  target="_blank"
+                  className="px-[15px] py-[6px]   font-[600] text-white bg-[#4aa3fa] rounded-md"
+                >
+                  Apply Now
+                </Link>
+              </div>
+              <div>
+                <div className="font-medium text-xl text-bold">
+                  ✍️ Full Description
+                </div>
+                <div
+                  dangerouslySetInnerHTML={formatDescription(
+                    currentJob.description
+                  )}
+                ></div>
+              </div>
               <Link
                 href={`${currentJob.application_link}`}
                 target="_blank"
-                className="px-[15px] py-[6px]   font-[600] text-white bg-[#4aa3fa] rounded-md"
+                className="px-[15px] py-[8px] w-fit   font-[600] text-white bg-[#4aa3fa] rounded-md"
               >
                 Apply Now
               </Link>
             </div>
-            <div>
-              <div className="font-medium text-xl text-bold">
-                ✍️ Full Description
-              </div>
-              <div
-                dangerouslySetInnerHTML={formatDescription(
-                  currentJob.description
-                )}
-              ></div>
-            </div>
-            <Link
-              href={`${currentJob.application_link}`}
-              target="_blank"
-              className="px-[15px] py-[8px] w-fit   font-[600] text-white bg-[#4aa3fa] rounded-md"
-            >
-              Apply Now
-            </Link>
           </div>
-        </div>
+          <div className="pt-5">
+            <Footer />
+          </div>
+        </Fragment>
       )}
       <div className="h-[calc(100%-100px)] flex items-center justify-center w-full">
         {loading && <OvalLoader />}
